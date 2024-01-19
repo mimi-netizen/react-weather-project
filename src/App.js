c615d0700f9c56bb60fb11ece64c2cedd2b10343
Celyne <dianakydd2@gmail.com>
2024-01-19 11:17:35 +0300

    Added bootstrap to my project

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        Coded by <a href="https://github.com/mimi-netizen">Celyne Kydd</a> and
-        is{" "}
-        <a href="https://github.com/mimi-netizen/react-weather-project">
-          open-sourced on Github
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          Coded by <a href="https://github.com/mimi-netizen">Celyne Kydd</a> and
+          is open-sourced on{" "}
+          <a href="https://github.com/mimi-netizen/react-weather-project">
+            Github
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
